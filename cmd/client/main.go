a822bb2b32be4738e04bd4746234182ea4ebe31f
e.zhydzetski <e.zhydzetski@gmail.com>
2022-10-03 10:36:00 +0300

    feat: make client configurable

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 	"os"
 	"os/signal"
@@ -13,12 +14,16 @@ import (
 )
 
 func errorAwareMain() error {
+	var serverAddr string
+	flag.StringVar(&serverAddr, "server-addr", "127.0.0.1:7777", "server address")
+	flag.Parse()
+
 	ctx := context.Background()
 
 	ctx, stop := signal.NotifyContext(ctx, os.Interrupt)
 	defer stop()
 
-	conn, err := tcp.Connect(ctx, "127.0.0.1:7777")
+	conn, err := tcp.Connect(ctx, serverAddr)
 	if err != nil {
 		return err
 	}
@@ -50,6 +55,7 @@ func errorAwareMain() error {
 func main() {
 	err := errorAwareMain()
 	if err != nil {
-		log.Println(err)
+		log.Println("ERROR:", err)
+		os.Exit(1)
 	}
 }
